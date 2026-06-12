9f2b76aadd6ce41f3d916ce8f1ee11417664c9b2
Asra Arshad <asraarshad04@gmail.com>
2026-06-13 00:56:22 +0500

    Refactor application routes

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -4,7 +4,8 @@ const {
   applyForJob,
   getMyApplications,
   getJobApplications,
-  updateApplicationStatus
+  updateApplicationStatus,
+  revealApplication
 } = require('../controllers/applicationController');
 
 const { protect, authorize } = require('../middleware/authMiddleware');
@@ -13,5 +14,6 @@ router.post('/', protect, authorize('student'), applyForJob);
 router.get('/my-applications', protect, authorize('student'), getMyApplications);
 router.get('/job/:jobId', protect, authorize('recruiter', 'admin'), getJobApplications);
 router.put('/:id/status', protect, authorize('recruiter', 'admin'), updateApplicationStatus);
+router.put('/:id/reveal', protect, authorize('recruiter', 'admin'), revealApplication);
 
 module.exports = router;
